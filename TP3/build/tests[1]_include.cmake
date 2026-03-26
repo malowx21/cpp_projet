0b87b7c302c93af253e6421b8af7086fa1e53482
Mohamed TROMBATI <Mohamed.Trombati@grenoble-inp.org>
2026-03-26 21:25:54 +0100

    build

--- TP3/build/tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/ensimag/cpp_projet/TP3/build/tests[1]_tests.cmake")
-  include("/home/ensimag/cpp_projet/TP3/build/tests[1]_tests.cmake")
-else()
-  add_test(tests_NOT_BUILT tests_NOT_BUILT)
-endif()
